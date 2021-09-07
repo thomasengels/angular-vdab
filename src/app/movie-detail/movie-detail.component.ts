24da47950272a018514c30ca34b240fd4a32da4a
Thomas Engels <thomas.engels92@gmail.com>
2021-09-07 11:47:36 +0200

    use regular activatedRoute instead of snapshot

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,6 +1,6 @@
 import { MoviesService } from './../services/movies.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRouteSnapshot } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 import { Movie } from '../models/movie.model';
 
 @Component({
@@ -12,10 +12,10 @@ export class MovieDetailComponent implements OnInit {
   movie: Movie;
 
   constructor(private moviesService: MoviesService,
-              private route: ActivatedRouteSnapshot) { }
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    const id: number = Number.parseInt(this.route.paramMap.get('id'), 10);
+    const id: number = Number.parseInt(this.route.snapshot.paramMap.get('id'), 10);
     this.movie = this.moviesService.getMovieById(id);
   }
 
