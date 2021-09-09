297d49acce90605c9ec177d252a3dde223fffcaa
Thomas Engels <thomas.engels92@gmail.com>
2021-09-09 15:21:39 +0200

    2_http

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const req = request.clone({headers: request.headers.set('Authorization', 'auth')});
+
+    return next.handle(req);
+  }
+}
